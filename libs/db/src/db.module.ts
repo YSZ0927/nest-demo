75c31c49a02a08dfd86f728319f9e51708776b2d
shuizeng Yu <shuizengyu@91jkys.com>
2020-07-28 17:18:04 +0800

    chore: add nestjs-typegoose

--- libs/db/src/db.module.ts
+++ libs/db/src/db.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DbService } from './db.service';
+import {TypegooseModule} from 'nestjs-typegoose';
 
 @Module({
+  imports: [
+    TypegooseModule.forRoot('mongodb://localhost:27017')
+  ],
   providers: [DbService],
   exports: [DbService],
 })
